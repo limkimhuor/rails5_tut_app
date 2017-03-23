8159a5c0ef44bdb2088125dfc79be3013f31f5f9
Lim Kimhuor <kimhuorlim@gmail.com>
2017-03-23 21:43:27 +0700

    Finish user signup and config ssl production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,11 @@ module ApplicationHelper
     base_title = "Ruby on Rails v5"
     page_title.empty? ? base_title : page_title + " | " + base_title
   end
+
+  def gravatar_for user, opts = { size: 80 }
+    gravatar_id = Digest::MD5::hexdigest user.email.downcase
+    size = opts[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag gravatar_url, alt: user.name, class: "gravatar"
+    end
 end
